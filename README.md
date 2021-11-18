3683139ec47cdb9d3f81d40d4ac5d095e6abce58
Julin Nowoczyn <nikkulin3@gmail.com>
2021-11-18 12:38:11 +0100

    simlink to setup_paf

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ###Symbolischer Link für paf-Packages:
 
+```
 ln -s ~/paf21-2/paf_ros/ ~/carla-ros-bridge/catkin_ws/src/
+```
 
 ###Architekturbeschreibung (WIP)
 
--- scripts/subscripts/_setup_paf.sh
+++ scripts/subscripts/_setup_paf.sh
@@ -4,6 +4,8 @@ sudo apt install -y python-is-python3 wmctrl ros-noetic-cv-bridge
 pip install carla_birdeye_view pre-commit
 
 cd ~/paf21-2/ || exit 1
+
+ln -s ~/paf21-2/paf_ros/ ~/carla-ros-bridge/catkin_ws/src/
 ./scripts/build_ros.sh --clean --no-build || exit 1
 
 pre-commit install
