6efbc57afc8f1f3117829597593a5225397841fc
Julin Nowoczyn <nikkulin3@gmail.com>
2021-11-23 09:15:17 +0100

    creating empty ros node

---
+++ paf_ros/paf_perception/src/segmented_lidar.py
@@ -0,0 +1,11 @@
+class SegmentedLidarNode(object):
+    def __init__(self):
+        ...
+
+    def start(self):
+        ...
+
+
+if __name__ == "__main__":
+    node = SegmentedLidarNode()
+    node.start()
