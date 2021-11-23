02e883ed95c914596828c8017981db84c644ba0c
Julin Nowoczyn <nikkulin3@gmail.com>
2021-11-23 10:58:41 +0100

    fix params

--- paf_ros/paf_validation/src/top_down_view.py
+++ paf_ros/paf_validation/src/top_down_view.py
@@ -81,22 +81,18 @@ class TopDownView(BirdViewProducer):
             target_size_ = gen._mask_size
             target_size = PixelDimensions(
                 width=int((target_size_.width + 2 * MAP_BOUNDARY_MARGIN) / 2),
-                height=int((target_size_.height + 2 *
-                           MAP_BOUNDARY_MARGIN) / 2),
+                height=int((target_size_.height + 2 * MAP_BOUNDARY_MARGIN) / 2),
             )
-        super(TopDownView, self).__init__(
-            client, target_size, pixels_per_meter, crop_type)
+        super(TopDownView, self).__init__(client, target_size, pixels_per_meter, crop_type)
 
     def produce(self, agent_vehicle: carla.Actor) -> BirdView:
         all_actors = actors.query_all(world=self._world)
         segregated_actors = actors.segregate_by_type(actors=all_actors)
-        agent_vehicle_loc = agent_vehicle.get_location(
-        ) if self.center_on_agent else Namespace(**{"x": 0, "y": 0})
+        agent_vehicle_loc = agent_vehicle.get_location() if self.center_on_agent else Namespace(**{"x": 0, "y": 0})
 
         # same as super class below
         self.masks_generator.disable_local_rendering_mode()
-        agent_global_px_pos = self.masks_generator.location_to_pixel(
-            agent_vehicle_loc)
+        agent_global_px_pos = self.masks_generator.location_to_pixel(agent_vehicle_loc)
         cropping_rect = CroppingRect(
             x=int(agent_global_px_pos.x - self.rendering_area.width / 2),
             y=int(agent_global_px_pos.y - self.rendering_area.height / 2),
@@ -111,21 +107,14 @@ class TopDownView(BirdViewProducer):
             ),
             dtype=np.uint8,
         )
-        masks[MaskPriority.ROAD.value] = self.full_road_cache[cropping_rect.vslice,
-                                                              cropping_rect.hslice]
-        masks[MaskPriority.LANES.value] = self.full_lanes_cache[cropping_rect.vslice,
-                                                                cropping_rect.hslice]
-        masks[MaskPriority.CENTERLINES.value] = self.full_centerlines_cache[cropping_rect.vslice,
-                                                                            cropping_rect.hslice]
-        rendering_window = RenderingWindow(
-            origin=agent_vehicle_loc, area=self.rendering_area)
+        masks[MaskPriority.ROAD.value] = self.full_road_cache[cropping_rect.vslice, cropping_rect.hslice]
+        masks[MaskPriority.LANES.value] = self.full_lanes_cache[cropping_rect.vslice, cropping_rect.hslice]
+        masks[MaskPriority.CENTERLINES.value] = self.full_centerlines_cache[cropping_rect.vslice, cropping_rect.hslice]
+        rendering_window = RenderingWindow(origin=agent_vehicle_loc, area=self.rendering_area)
         self.masks_generator.enable_local_rendering_mode(rendering_window)
-        masks = self._render_actors_masks(
-            agent_vehicle, segregated_actors, masks)
-        masks[MaskPriority.GLOBAL_PATH] = self._create_path_mask(
-            self.global_path)
-        masks[MaskPriority.LOCAL_PATH] = self._create_path_mask(
-            self.local_path)
+        masks = self._render_actors_masks(agent_vehicle, segregated_actors, masks)
+        masks[MaskPriority.GLOBAL_PATH] = self._create_path_mask(self.global_path)
+        masks[MaskPriority.LOCAL_PATH] = self._create_path_mask(self.local_path)
         cropped_masks = self.apply_agent_following_transformation_to_masks(
             agent_vehicle,
             masks,
@@ -145,20 +134,16 @@ class TopDownView(BirdViewProducer):
         crop_with_car_in_the_center = masks
         masks_n, h, w = crop_with_car_in_the_center.shape
         rotation_center = Coord(x=w // 2, y=h // 2)
-        crop_with_centered_car = np.transpose(
-            crop_with_car_in_the_center, axes=(1, 2, 0))
+        crop_with_centered_car = np.transpose(crop_with_car_in_the_center, axes=(1, 2, 0))
         rotated = rotate(crop_with_centered_car, angle, center=rotation_center)
         rotated = np.transpose(rotated, axes=(2, 0, 1))
         half_width = self.target_size.width // 2
-        hslice = slice(rotation_center.x - half_width,
-                       rotation_center.x + half_width)
+        hslice = slice(rotation_center.x - half_width, rotation_center.x + half_width)
         if self._crop_type is BirdViewCropType.FRONT_AREA_ONLY:
-            vslice = slice(rotation_center.y -
-                           self.target_size.height, rotation_center.y)
+            vslice = slice(rotation_center.y - self.target_size.height, rotation_center.y)
         elif self._crop_type is BirdViewCropType.FRONT_AND_REAR_AREA:
             half_height = self.target_size.height // 2
-            vslice = slice(rotation_center.y - half_height,
-                           rotation_center.y + half_height)
+            vslice = slice(rotation_center.y - half_height, rotation_center.y + half_height)
         else:
             raise NotImplementedError
         assert (
@@ -184,11 +169,9 @@ class TopDownView(BirdViewProducer):
         mask = self.masks_generator.make_empty_mask()
         if path is None:
             return mask
-        points = [self.masks_generator.location_to_pixel(
-            Namespace(**{"x": x, "y": y})) for x, y in path]
+        points = [self.masks_generator.location_to_pixel(Namespace(**{"x": x, "y": y})) for x, y in path]
         points = np.array([[p.x, p.y] for p in points])
-        mask = cv2.polylines(mask, [points.reshape(
-            (-1, 1, 2))], False, COLOR_ON, self.path_width_px)
+        mask = cv2.polylines(mask, [points.reshape((-1, 1, 2))], False, COLOR_ON, self.path_width_px)
         return mask
 
     def _render_actors_masks(
@@ -198,18 +181,14 @@ class TopDownView(BirdViewProducer):
         masks: np.ndarray,
     ) -> np.ndarray:
         # same as super class with new MaskPriorities
-        lights_masks = self.masks_generator.traffic_lights_masks(
-            segregated_actors.traffic_lights)
+        lights_masks = self.masks_generator.traffic_lights_masks(segregated_actors.traffic_lights)
         red_lights_mask, yellow_lights_mask, green_lights_mask = lights_masks
         masks[MaskPriority.RED_LIGHTS.value] = red_lights_mask
         masks[MaskPriority.YELLOW_LIGHTS.value] = yellow_lights_mask
         masks[MaskPriority.GREEN_LIGHTS.value] = green_lights_mask
-        masks[MaskPriority.AGENT.value] = self.masks_generator.agent_vehicle_mask(
-            agent_vehicle)
-        masks[MaskPriority.VEHICLES.value] = self.masks_generator.vehicles_mask(
-            segregated_actors.vehicles)
-        masks[MaskPriority.PEDESTRIANS.value] = self.masks_generator.pedestrians_mask(
-            segregated_actors.pedestrians)
+        masks[MaskPriority.AGENT.value] = self.masks_generator.agent_vehicle_mask(agent_vehicle)
+        masks[MaskPriority.VEHICLES.value] = self.masks_generator.vehicles_mask(segregated_actors.vehicles)
+        masks[MaskPriority.PEDESTRIANS.value] = self.masks_generator.pedestrians_mask(segregated_actors.pedestrians)
         return masks
 
     def as_rgb(self, birdview: BirdView):
@@ -230,8 +209,7 @@ class TopDownView(BirdViewProducer):
                 rgb_color = 255 - r, 255 - g, 255 - b
             rgb_canvas[nonzero_indices(birdview[mask_type])] = rgb_color
         if not self.dark_mode:
-            rgb_canvas = np.where(
-                rgb_canvas.any(-1, keepdims=True), rgb_canvas, 255)
+            rgb_canvas = np.where(rgb_canvas.any(-1, keepdims=True), rgb_canvas, 255)
         return rgb_canvas
 
 
@@ -243,7 +221,6 @@ class TopDownRosNode(object):
         self.actor = _actor
         self._init()
         rospy.init_node(self.params["node"], anonymous=True)
-        print(self.actor.type_id)
         self.pub = rospy.Publisher(self.params["topic"], Image, queue_size=1)
 
     def _init(self):
@@ -259,8 +236,7 @@ class TopDownRosNode(object):
             dark_mode=self.params["dark_mode"],
         )
         # todo temporary
-        self.producer.set_path([[-500, -500], [500, 500]],
-                               [[500, -500], [-500, 500]])
+        self.producer.set_path([[-500, -500], [500, 500]], [[500, -500], [-500, 500]])
         print(f"top_down_view tracking {self.actor.type_id}")
 
     def produce_map(self):
@@ -275,8 +251,7 @@ class TopDownRosNode(object):
             rgb = self.produce_map()
             self.pub.publish(self.br.cv2_to_imgmsg(rgb, "rgb8"))
             if not first_run:
-                rospy.logwarn_once(
-                    f"[top_down_view] calc_time={time.perf_counter() - t0}s")
+                rospy.logwarn_once(f"[top_down_view] calc_time={time.perf_counter() - t0}s")
             rate.sleep()
             first_run = False
 
@@ -286,12 +261,10 @@ if __name__ == "__main__":
     _actors = client.get_world().get_actors()
     vehicles = []
     for actor in _actors:
-        rospy.logerr(actor.type_id)
         if "vehicle." in actor.type_id:
             vehicles.append(actor)
-        if "role_name" in actor.attributes and actor.attributes["role_name"] == rospy.get_param(
-            "/top_down_view/role_name"
-        ):
+        if "role_name" in actor.attributes and actor.attributes["role_name"] == rospy.get_param("role_name"):
+            rospy.logwarn(f"Tracking {actor.type_id} ({actor.attributes['role_name']})")
             break
     else:
         if not len(vehicles):
