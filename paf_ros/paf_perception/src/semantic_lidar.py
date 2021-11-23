b0342b35b6937a71c58cc4326dc7fa2b13e17707
Julin Nowoczyn <nikkulin3@gmail.com>
2021-11-23 16:33:10 +0100

    second draft

--- paf_ros/paf_perception/src/semantic_lidar.py
+++ paf_ros/paf_perception/src/semantic_lidar.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# from time import perf_counter
+from nav_msgs.msg import Odometry
 
 import rospy
 from sensor_msgs.msg import PointCloud2
@@ -36,23 +36,20 @@ class SemanticLidarNode(object):
 
     def __init__(self):
         rospy.init_node("semantic_lidar", anonymous=True)
-        topic = "/carla/ego_vehicle/semantic_lidar/lidar1/point_cloud"
-        self._lidar_subscriber = rospy.Subscriber(topic, PointCloud2, self.process_lidar_semantic)
-        topic = "/carla/ego_vehicle/lidar/lidar2/point_cloud"
-        self._lidar_subscriber2 = rospy.Subscriber(topic, PointCloud2, self.process_lidar_standard)
+        topic1 = "/carla/ego_vehicle/semantic_lidar/lidar1/point_cloud"
+        topic2 = "/carla/ego_vehicle/odometry"
+        self._lidar_subscriber = rospy.Subscriber(topic1, PointCloud2, self.process_lidar_semantic)
+        self._odometry_subcriber = rospy.Subscriber(topic2, Odometry, self.process_odometry)
         self.ignored_indices = [0, 6, 7] + list(range(13, 23))
+        self.position = None
 
-    def process_lidar_standard(self, msg: PointCloud2):
-        points = pc2.read_points(msg)
-        counter = 0
-        for point in points:
-            counter += 1
-            d, r, dynamic, intensity = point
-        print("aue")
+    def process_odometry(self, msg: Odometry):
+        self.position = np.array(msg.pose.pose.position.x, msg.pose.pose.position.y)
 
     def process_lidar_semantic(self, msg: PointCloud2):
+        if self.position is None:
+            return
         points = pc2.read_points(msg, skip_nans=True)
-        # t0 = perf_counter()
         point_count = 0
         objects = {}
         static_objects = {}
@@ -68,20 +65,36 @@ class SemanticLidarNode(object):
                 static_objects[object_tag].append((x, y, z))
             else:
                 if object_idx not in objects:
-                    objects[object_idx] = {"x": [], "y": [], "tag": object_tag}
+                    objects[object_idx] = {"x": [], "y": [], "d": [], "tag": object_tag}
                 objects[object_idx]["x"].append(x)
                 objects[object_idx]["y"].append(y)
-        # t_perf = perf_counter() - t0
-        # t_perf_per_point = t_perf / point_count
-        # temp = [len(x["x"]) for x in objects.values()]
+                objects[object_idx]["d"].append(np.sqrt(x ** 2 + y ** 2))
         objects_min_max = {}
         for k, obj in objects.items():
-            objects_min_max[f"{obj['tag']}_{k}"] = {
-                "max": (np.max(obj["x"]), np.max(obj["y"])),
-                "min": (np.min(obj["x"]), np.min(obj["y"])),
-            }
+            a_max_x, a_max_y = np.argmax(obj["x"]), np.argmax(obj["y"])
+            a_min_x, a_min_y = np.argmin(obj["x"]), np.argmin(obj["y"])
+            a_min_d = np.argmin(obj["d"])
+            d_min = obj["d"][a_min_d]
 
-        pass
+            poi = [
+                (obj["x"][a_max_x], obj["y"][a_max_x], obj["d"][a_max_x]),
+                (obj["x"][a_min_x], obj["y"][a_min_x], obj["d"][a_min_x]),
+                (obj["x"][a_max_y], obj["y"][a_max_y], obj["d"][a_max_y]),
+                (obj["x"][a_min_y], obj["y"][a_min_y], obj["d"][a_min_y]),
+            ]
+
+            poi = sorted(poi, key=lambda _x: np.sqrt(_x[0] ** 2 + _x[1] ** 2), reverse=True)
+            bound_1, bound_2 = poi[:2]
+            bound_1 = np.array(bound_1[:2]) / bound_1[2] * d_min + self.position
+            bound_2 = np.array(bound_2[:2]) / bound_2[2] * d_min + self.position
+            closest = np.array((obj["x"][a_min_d], obj["y"][a_min_d])) + self.position
+            poi = (tuple(bound_1), tuple(bound_2), tuple(closest))
+            objects_min_max[f"{obj['tag']}_{k}"] = poi
+
+        self.publish_dynamic_object_information(objects_min_max)
+
+    def publish_dynamic_object_information(self, objects_min_max):
+        pass  # todo
 
     def start(self):
         rate = rospy.Rate(1)
