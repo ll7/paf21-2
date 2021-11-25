508e50ee781e98ccf0830576ba6039d6daa6ca03
Julin Nowoczyn <nikkulin3@gmail.com>
2021-11-25 09:35:54 +0100

    sensors, cleanup + refactor

--- paf_ros/paf_perception/src/semantic_lidar.py
+++ paf_ros/paf_perception/src/semantic_lidar.py
@@ -17,6 +17,8 @@ class SemanticLidarNode(object):
     TAGS = {int(k): v for k, v in rospy.get_param("semantic_tags").items()}
     MIN_DIST_IS_SELF = 0  # not needed if z >= 2.4
     MIN_OBJ_DIFF_DIST = 5
+    SENSOR_X_OFFSET = -1
+    SKIP_NORMALIZING_BOUND_DIST = False
 
     def __init__(self):
         rospy.init_node("semantic_lidar", anonymous=True)
@@ -37,15 +39,15 @@ class SemanticLidarNode(object):
 
     def _incoming_change(self, msg_1: Odometry, msg_2: PointCloud2):
         t0 = perf_counter()
-        self.process_odometry(msg_1)
-        self.process_lidar_semantic(msg_2)
+        self._process_odometry(msg_1)
+        self._process_lidar_semantic(msg_2)
         time = perf_counter()
         rospy.logwarn_throttle(10, f"[semantic lidar] max fps={1 / (time - t0)}")
         rospy.logwarn_throttle(10, f"[semantic lidar] current fps={1 / (time - self.frame_time)}")
         self.frame_time = time
 
-    def process_odometry(self, msg: Odometry):
-        self.xy_position = np.array([msg.pose.pose.position.x, -msg.pose.pose.position.y])
+    def _process_odometry(self, msg: Odometry):
+        self.xy_position = np.array([msg.pose.pose.position.x + self.SENSOR_X_OFFSET, -msg.pose.pose.position.y])
         current_pose = msg.pose.pose
         quaternion = (
             current_pose.orientation.x,
@@ -59,81 +61,98 @@ class SemanticLidarNode(object):
         self.cos = np.cos(-self.z_orientation)
         self.sin = np.sin(-self.z_orientation)
 
-    def transform_to_relative_world_pos(self, leftwards, backwards):
+    def _transform_to_relative_world_pos(self, leftwards, backwards):
         x = leftwards
         y = -backwards
         x, y = x * self.cos - y * self.sin, y * self.cos + x * self.sin
         return x, y
 
-    def process_lidar_semantic(self, msg: PointCloud2):
+    def _process_lidar_semantic(self, msg: PointCloud2):
         if self.xy_position is None:
             return
         points = pc2.read_points(msg, skip_nans=True)
-        point_count = 0
+        sorted_points = self._process_lidar_points_by_tag_and_idx(points)
+        bounds_by_tag = self._process_sorted_points_calculate_bounds(sorted_points)
+        self._publish_object_information(bounds_by_tag)
+
+    def _process_sorted_points_calculate_bounds(self, sorted_points):
+        bounds_by_tag = {}
+        for obj_idx, pts_and_tag in sorted_points.items():
+            tag = pts_and_tag["tag"]
+            if tag not in bounds_by_tag:
+                bounds_by_tag[tag] = []
+            for i, pts in enumerate(pts_and_tag["pts"]):
+                a_max_x, a_max_y, a_max_d = np.argmax(pts, axis=0)
+                a_min_x, a_min_y, a_min_d = np.argmin(pts, axis=0)
+
+                # bounds: select two most distant points of interest (poi)
+                poi = [pts[a_max_x], pts[a_min_x], pts[a_max_y], pts[a_min_y]]
+                poi = sorted(poi, key=lambda _x: _x[-1], reverse=True)
+                bound_1, bound_2 = poi[:2]
+
+                # closest: select closest point to sensor
+                closest = np.array((pts[a_min_d][:2]))
+                if self.SKIP_NORMALIZING_BOUND_DIST:
+                    # use original bound points
+                    bound_2 = np.array(bound_2[:2])
+                    bound_1 = np.array(bound_1[:2])
+                else:
+                    # shorten bound vectors to len(closest)
+                    d_min = pts[a_min_d][-1]
+                    bound_1 = np.array(bound_1[:2]) / bound_1[2] * d_min
+                    bound_2 = np.array(bound_2[:2]) / bound_2[2] * d_min
+                poi = [bound_1, bound_2, closest]
+                poi = [p + self.xy_position for p in poi]
+                bounds_by_tag[tag].append(poi)
+        return bounds_by_tag
+
+    def _process_lidar_points_by_tag_and_idx(self, points):
         objects = {}
         for point in points:
             leftwards, backwards, downwards, cos_inc_angle, object_idx, object_tag = point
-            x, y = self.transform_to_relative_world_pos(leftwards, backwards)
-            point_count += 1
+            x, y = self._transform_to_relative_world_pos(leftwards, backwards)
             if object_tag in self.ignored_indices:
                 continue
             object_tag = self.TAGS[object_tag]
             if object_idx == 0:
                 continue
             else:
-                object_idx = str(object_idx)
-                d = np.sqrt(x ** 2 + y ** 2)
+                d = self._dist((x, y))
                 if d < self.MIN_DIST_IS_SELF:
                     continue
                 if object_idx not in objects:
-                    objects[object_idx] = {"objects": [], "tag": object_tag}
-                for idx, objects_with_idx in enumerate(objects[object_idx]["objects"]):
-                    _x, _y, _ = objects_with_idx[0]
-                    _d = np.sqrt((x - _x) ** 2 + (y - _y) ** 2)
+                    objects[object_idx] = {"pts": [], "tag": object_tag}
+                for idx, objects_with_idx in enumerate(objects[object_idx]["pts"]):
+                    _x, _y, _ = objects_with_idx[-1]
+                    _d = self._dist((x, y), (_x, _y))
                     if _d < self.MIN_OBJ_DIFF_DIST:
-                        objects[object_idx]["objects"][idx].append([x, y, d])
+                        objects[object_idx]["pts"][idx].append([x, y, d])
                         break
                 else:
-                    objects[object_idx]["objects"].append([[x, y, d]])
-        objects_min_max = {}
-        for obj_idx, obj_and_tag in objects.items():
-            tag = obj_and_tag["tag"]
-            obj: dict
-            for i, pts in enumerate(obj_and_tag["objects"]):
-                a_max_x, a_max_y, a_max_d = np.argmax(pts, axis=0)
-                a_min_x, a_min_y, a_min_d = np.argmin(pts, axis=0)
-                d_min = pts[a_min_d][-1]
-                poi = [pts[a_max_x], pts[a_min_x], pts[a_max_y], pts[a_min_y]]
+                    objects[object_idx]["pts"].append([[x, y, d]])
+        return objects
 
-                poi = sorted(poi, key=lambda _x: np.sqrt(_x[0] ** 2 + _x[1] ** 2), reverse=True)
-                bound_1, bound_2 = poi[:2]
-                closest = np.array((pts[a_min_d][:2]))
-                bound_1 = np.array(bound_1[:2]) / bound_1[2] * d_min + self.xy_position
-                bound_2 = np.array(bound_2[:2]) / bound_2[2] * d_min + self.xy_position
-                # bound_2 = np.array(bound_2[:2]) + self.xy_position
-                # bound_1 = np.array(bound_1[:2]) + self.xy_position
-                closest += self.xy_position
-                poi = [bound_1, bound_2, closest]
-                if tag not in objects_min_max:
-                    objects_min_max[tag] = {}
-                objects_min_max[tag][obj_idx] = poi
-        self.publish_dynamic_object_information(objects_min_max)
-
-    def publish_dynamic_object_information(self, objects_min_max):
+    def _publish_object_information(self, bounds_by_tag):
         header = Header()
         header.stamp = rospy.Time.now()
-        for tag, values in objects_min_max.items():
+        for tag, values in bounds_by_tag.items():
             obstacles = PafObstacleList()
             obstacles.type = tag
             obstacles.header = header
             obstacles.obstacles = []
-            for i, (bound_1, bound_2, closest) in values.items():
+            for i, (bound_1, bound_2, closest) in enumerate(values):
                 obstacle = PafObstacle()
                 obstacle.bound_1 = bound_1
                 obstacle.bound_2 = bound_2
                 obstacle.closest = closest
                 obstacles.obstacles.append(obstacle)
             self._obstacle_publisher.publish(obstacles)
+
+    @staticmethod
+    def _dist(p1, p2=(0, 0)):
+        x1, y1 = p1
+        x2, y2 = p2
+        return np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
     @staticmethod
     def start(rate=100):
