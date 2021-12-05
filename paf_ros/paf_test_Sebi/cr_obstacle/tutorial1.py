06c86639883dd547a10a36538e7d0fe8b1700fa5
Marco-Mueller <mue.marco10@gmail.com>
2021-12-05 15:36:55 +0100

    new xml examples

--- paf_ros/paf_test_Sebi/cr_obstacle/tutorial1.py
+++ paf_ros/paf_test_Sebi/cr_obstacle/tutorial1.py
@@ -1,15 +1,23 @@
-
 import os
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+# from IPython import display
 
-from commonroad.common.file_reader import CommonRoadFileReader
-from commonroad.visualization.mp_renderer import MPRenderer
+# # import functions to read xml file and visualize commonroad objects
+# from commonroad.common.file_reader import CommonRoadFileReader
+# from commonroad.visualization.mp_renderer import MPRenderer
 
-# load the exemplary CommonRoad scenario using the CommonRoad file reader
-scenario, planning_problem_set = CommonRoadFileReader('DEU_Hhr-1_1.xml').open()
+# # generate path of the file to be opened
+# file_path = "ZAM_Tutorial-1_1_T-1.xml"
 
-"""# plot the scenario
-rnd = MPRenderer(figsize=(25, 10))
-scenario.draw(rnd)
-planning_problem_set.draw(rnd)
-rnd.render()"""
+# # read in the scenario and planning problem set
+# scenario, planning_problem_set = CommonRoadFileReader(file_path).open()
+
+# # plot the scenario for 40 time step, here each time step corresponds to 0.1 second
+# for i in range(0, 40):
+#     plt.figure(figsize=(25, 10))
+#     rnd = MPRenderer()
+#     # plot the scenario at different time step
+#     scenario.draw(rnd, draw_params={'time_begin': i})
+#     # plot the planning problem set
+#     planning_problem_set.draw(rnd)
+#     rnd.render()
