9c929ed30e1011350993fc7ffee28f132a60a55b
jkruess <likeace@t-online.de>
2021-12-16 17:22:53 +0100

    Adapted MapManager.py

--- paf_ros/paf_planning/src/classes/MapManager.py
+++ paf_ros/paf_planning/src/classes/MapManager.py
@@ -1,38 +1,31 @@
-from commonroad.planning.planning_problem import PlanningProblemSet
-from commonroad.scenario.scenario import Scenario
+import rospy
+from os.path import expanduser
+
 from commonroad.common.file_reader import CommonRoadFileReader
 
 
 class MapManager:
-    def __init__(self, init_rospy: bool = False):
-
-        self.map_file_path: str = None
-
-        self.scenario: Scenario = None
-        self.planning_problem_set: PlanningProblemSet = None
-
-    def _load_scenario(self, map_number: int, rules: bool):
+    @staticmethod
+    def get_scenario(rules=True):
         """
         Loads the commonroad scenario with or without traffic rules of town with number map_number
+
+        Args:
+            rules (bool): Defines which driving mode the map should be loaded for
+
+        Returns:
+            Scenario: CommonRoad-Scenario of current town
         """
-        map_files = {
-            1: "DEU_Town01-1_1_T-1.xml",
-            2: "DEU_Town02-1_1_T-1.xml",
-            3: "DEU_Town03-1_1_T-1.xml",
-            4: "DEU_Town04-1_1_T-1.xml",
-            5: "DEU_Town05-1_1_T-1.xml",
-            6: "DEU_Town06-1_1_T-1.xml",
-            7: "DEU_Town07-1_1_T-1.xml",
-            10: "DEU_Town10HD-1_1_T-1.xml",
-        }
-
+        map_name = None
+        try:
+            map_name = rospy.get_param("/carla/town")
+        except KeyError:
+            rospy.logerr("MapManager: Town parameter not set.")
+            exit(1)
+        map_file_name = "DEU_" + map_name + "-1_1_T-1.xml"
         if rules:
-            self.map_file_path = "Maps/Rules/" + map_files.get(map_number)
+            map_file_path = "Maps/Rules/" + map_file_name
         else:
-            self.map_file_path = "Maps/No Rules/" + map_files.get(map_number)
-
-        # read in the scenario and planning problem set
-        self.scenario, self.planning_problem_set = CommonRoadFileReader(self.map_file_path).open()
-
-    def get_scenario(self):
-        return self.scenario
+            map_file_path = "Maps/No Rules/" + map_file_name
+        scenario, _ = CommonRoadFileReader(expanduser(f"~/.ros/{map_file_path}")).open()
+        return scenario
