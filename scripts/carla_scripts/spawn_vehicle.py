60333b32e4c90f2520dc33b1708c1ca5af0db71d
sebi <sebi@mail>
2022-01-12 19:30:06 +0100

    update

--- scripts/carla_scripts/spawn_vehicle.py
+++ scripts/carla_scripts/spawn_vehicle.py
@@ -12,6 +12,19 @@ vehicle_bp = blueprint_library.find("vehicle.tesla.model3")
 spawn_points = world.get_map().get_spawn_points()
 model3_spawn_point = np.random.choice(spawn_points)
 
+
+target_waypoints = [100, 207.5, 1.5]
+
+
 # spawn position
-transform = carla.Transform(carla.Location(x=12, y=207.5, z=1.5), carla.Rotation(yaw=180))
-actor = world.spawn_actor(vehicle_bp, transform)
+start = carla.Transform(carla.Location(x=12, y=207.5, z=1.5), carla.Rotation(yaw=180))
+vehicle = world.spawn_actor(vehicle_bp, start)
+
+# custom_controller = VehiclePIDController(vehicle, args_lateral = {'K_P': 1, 'K_D': 0.0, 'K_I': 0},
+# 	args_longitudinal = {'K_P': 1, 'K_D': 0.0, 'K_I': 0.0})
+
+# ticks_to_track = 20
+# current_waypoint = start
+# for i in range(ticks_to_track):
+# 	control_signal = custom_controller.run_step(1, target_waypoint)
+# 	vehicle.apply_control(control_signal)
