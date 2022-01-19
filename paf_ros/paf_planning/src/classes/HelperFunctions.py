signs fixed

paf_ros/paf_planning/src/classes/HelperFunctions.py
@@ -31,7 +31,10 @@ def closest_index_of_point_list(pts_list: List[Point2D], target_pt, acc: int = 1
         return -1, -1
     if hasattr(target_pt, "x"):
         target_pt = (target_pt.x, target_pt.y)
-    distances = [dist([p.x, p.y], target_pt) for p in pts_list[::acc]]
+    if hasattr(pts_list[0], "x"):
+        distances = [dist([p.x, p.y], target_pt) for p in pts_list[::acc]]
+    else:
+        distances = [dist(p, target_pt) for p in pts_list[::acc]]
     if len(distances) == 0:
         return len(pts_list) - 1
     idx = int(np.argmin(distances))
@@ -48,7 +51,31 @@ def k_closest_indices_of_point_in_list(k: int, pts_list: List[Point2D], target_p
     return idx * acc, [distances[i] for i in idx]
 
 
-def expand_sparse_list(sparse_list, to_len, fill_value=None):
+def expand_sparse_list(sparse_list, points_current, points_target, fill_value=None, indices_new=None):
+    assert len(sparse_list) == len(points_current)
+    if indices_new is None:
+        indices_new = [closest_index_of_point_list(points_target, pt)[0] for pt in points_current]
+
+    out = [fill_value for _ in points_target]
+
+    for v, idx in zip(sparse_list, indices_new):
+        idx = idx + 5
+        if idx >= len(out):
+            break
+        elif 0 <= idx:
+            out[idx] = v
+
+    filler = sparse_list[0]
+    if fill_value is None:
+        for i, v in enumerate(out):
+            if v is None:
+                out[i] = filler
+            else:
+                filler = v
+    return out, indices_new
+
+
+def expand_sparse_list2(sparse_list, to_len, fill_value=None):
     if len(sparse_list) == 0 or to_len < len(sparse_list):
         raise ValueError
     ratio = to_len / (len(sparse_list))
@@ -63,7 +90,7 @@ def expand_sparse_list(sparse_list, to_len, fill_value=None):
             expanded_list.append(sp)
             if len(expanded_list) > to_len:
                 expanded_list[to_len - 1] = sp
-        new_i = int(i * ratio)
+        new_i = int((i + 2) * ratio)
         for _ in range(len(expanded_list), new_i):
             expanded_list.append(filler)
 

paf_ros/paf_planning/src/classes/LocalPath.py
@@ -197,10 +197,12 @@ class LocalPath:
                 dist_pts(self.global_path.route.sections[section_from].points[0], self._sparse_local_path[end_index])
                 > 15
             ):
-                rospy.logerr(f"[LocalPath] previous idx not possible ({prev_idx} of {len(self._sparse_local_path)})")
+                rospy.logerr(
+                    f"[local planner] previous idx not possible ({prev_idx} of {len(self._sparse_local_path)})"
+                )
                 prev_idx = -1  # sanity check
         if prev_idx <= 0 or section_from < 0:
-            # rospy.logwarn("[LocalPath] calculating position on next centerline")
+            # rospy.logwarn("[local planner] calculating position on next centerline")
             section_from, current_lane = self.global_path.get_section_and_lane_indices(from_position)
             if section_from > 0:
                 point, current_speed, signals = self.global_path.get_local_path_values(section_from, current_lane)
@@ -208,7 +210,7 @@ class LocalPath:
                 sparse_local_path_speeds = [current_speed]
                 sparse_traffic_signals = [signals]
 
-        rospy.logwarn((current_lane, target_lane, section_from, section_target))
+        # rospy.logwarn((current_lane, target_lane, section_from, section_target))
         if section_from < 0 or section_target < 0:
             rospy.logerr_throttle(
                 1,
@@ -216,7 +218,7 @@ class LocalPath:
                 f"{self.global_path.route.target}, {len(self.global_path)}",
             )
             self.message = local_path
-            return local_path
+            return local_path, 0, 0
 
         self._local_path_start_section = section_from
         end_idx_lane_change = 0
@@ -382,9 +384,12 @@ class LocalPath:
             _points = calc_spline_course_from_point_list_grouped(sparse_local_path[::3], ds=self.STEP_SIZE, group_no=3)
             if not np.isnan(_points).any():
                 try:
+                    old_pts = points
                     points = xy_to_pts(_points)
-                    traffic_signals = expand_sparse_list(sparse_traffic_signals, len(points), fill_value=[])
-                    speed_limit = expand_sparse_list(sparse_local_path_speeds, len(points))
+                    traffic_signals, indices = expand_sparse_list(
+                        sparse_traffic_signals, old_pts, _points, fill_value=[]
+                    )
+                    speed_limit, _ = expand_sparse_list(sparse_local_path_speeds, old_pts, _points, indices_new=indices)
                 except ValueError:
                     points = sparse_local_path
 
@@ -406,7 +411,13 @@ class LocalPath:
         self.message = local_path
         self.traffic_signals = sparse_traffic_signals
 
-        return local_path
+        rospy.loginfo_throttle(
+            5,
+            f"[local planner] planned local path with length={distance_planned} "
+            f"over {len(sparse_local_path)} sections local_points: {len(local_path.points)}",
+        )
+
+        return local_path, section_from, section_target
 
     def _update_target_speed(self, local_path, speed_limit, traffic_signals):
         speed = self.speed_calc.get_curve_speed(local_path)
@@ -414,8 +425,27 @@ class LocalPath:
         if self.rules_enabled:
             speed_limit = np.clip(speed_limit, 33 / 3.6, 666 / 3.6)
             speed = np.clip(speed, 0, speed_limit)
-            # speed = self.speed_calc.add_stop_events(speed, traffic_signals, target_speed=0, buffer_m=6, shift_m=-3)
-            # speed = self.speed_calc.add_roll_events(speed, traffic_signals, target_speed=0, buffer_m=6, shift_m=-3)
+            # speed, indices2 = self.speed_calc.add_roll_events(speed, traffic_signals, target_speed=0, buffer_m=10,
+            #                                                   shift_m=0)
+            events = SpeedCalculator.ROLLING_EVENTS + SpeedCalculator.QUICK_BRAKE_EVENTS
+            speed, indices1 = self.speed_calc.add_stop_events(
+                speed, traffic_signals, target_speed=0, buffer_m=20, shift_m=-4, events=events
+            )
+
+            # if not hasattr(self, "pts_stop"):
+            #     self.pts_stop = []
+            #     self.pts_roll = []
+            #
+            # self.pts_stop += [local_path[i] for i in indices1 if local_path[i] not in self.pts_stop]
+            # # self.pts_roll = [local_path[i] for i in indices2 if local_path[i] not in self.pts_roll]
+            # # if len(indices1) > 0:
+            # #     rospy.logwarn(f"stops: {[indices1]}")
+            # # rospy.logwarn(f"rolls: {indices2}")
+            # self._draw_path_pts([local_path[i] for i, sp in enumerate(speed) if sp < .1][::10] +
+            # self.pts_stop, "uartenduiaerüöäp",
+            #                     color=(117, 34, 34))  # yellow
+            # self._draw_path_pts(self.pts_stop, "uartenduiaerüöäp",
+            #                     color=(117, 34, 34))  # dark red # (209, 203, 13) yellow
 
         speed = self.speed_calc.add_linear_deceleration(speed)
         return speed
@@ -435,9 +465,9 @@ class LocalPath:
         if left is None and straight is None and right is None:
             raise ValueError
 
-        left_percent = int(left is not None) * 1
-        right_percent = int(right is not None) * 1
-        straight_percent = int(straight is not None) * 50
+        left_percent = int(left is not None) * 100
+        right_percent = int(right is not None) * 100
+        straight_percent = int(straight is not None) * 1
 
         # l_pts, l_speed, l_signs = left
         # s_pts, s_speed, s_signs = straight

paf_ros/paf_planning/src/classes/SpeedCalculator.py
@@ -11,7 +11,7 @@ class SpeedCalculator:
     MAX_SPEED = 250 / 3.6
     MIN_SPEED = 35 / 3.6
     CURVE_FACTOR = 2  # higher value = more drifting
-    MAX_DECELERATION = 40  # m/s^2, higher value = later and harder braking
+    MAX_DECELERATION = 30  # m/s^2, higher value = later and harder braking
     SPEED_LIMIT_MULTIPLIER = 1
 
     # percentage of max_deceleration (last x% meters, MAX_DECELERATION/2 is used)
@@ -186,6 +186,7 @@ class SpeedCalculator:
         buffer_idx = int(buffer_m / self._step_size)
         shift_idx = int(shift_m / self._step_size)
         speed_limit = np.ones_like(speed) * 1000
+        indices = []
 
         if events is None:
             events = self.QUICK_BRAKE_EVENTS
@@ -196,14 +197,28 @@ class SpeedCalculator:
                     i0, i1 = i + shift_idx, i + buffer_idx + shift_idx
                     i1 += 1  # i1 is excluded otherwise
                     speed_limit[i0:i1] = target_speed
-        return np.clip(speed, 0, speed_limit)
+                    indices.append(i)
 
-    def remove_stop_event(self, speed, start_idx=0, buffer_m=5, speed_limit=None):
-        buffer_idx = int(buffer_m / self._step_size)
+        return np.clip(speed, 0, speed_limit), indices
+
+    @staticmethod
+    def remove_stop_event(speed, start_idx=0, speed_limit=None):
         if speed_limit is None:
-            speed_limit = self.MAX_SPEED
-        end_idx = start_idx + buffer_idx
-        speed[start_idx:end_idx] = speed_limit
+            speed_limit = SpeedCalculator.CITY_SPEED_LIMIT
+
+        num_max = 200
+        num_min = 100
+        had_zero = False
+        for i, sp in enumerate(speed[start_idx:]):
+            num = i
+            i += start_idx
+
+            if had_zero and num > num_min and (sp > 1 or num > num_max):
+                break
+            elif sp < 0.1:
+                had_zero = True
+            num += 1
+            speed[i] = speed_limit
         return speed
 
     def add_roll_events(

paf_ros/paf_planning/src/paf_local_planner.py
@@ -16,11 +16,13 @@ from paf_messages.msg import (
     PafLaneletRoute,
     PafTopDownViewPointSet,
     PafSpeedMsg,
+    PafRouteSection,
 )
 from classes.MapManager import MapManager
 from classes.GlobalPath import GlobalPath
 from classes.LocalPath import LocalPath
 from classes.HelperFunctions import closest_index_of_point_list, dist_pts
+from classes.SpeedCalculator import SpeedCalculator
 from std_msgs.msg import Bool, Empty
 from tf.transformations import euler_from_quaternion
 
@@ -30,7 +32,7 @@ class LocalPlanner:
 
     UPDATE_HZ = 10
     REPLAN_THROTTLE_SEC = 5
-    END_OF_ROUTE_REACHED_DIST = 5
+    END_OF_ROUTE_REACHED_DIST = 2
     # END_OF_ROUTE_SPEED = 5
     # MAX_ANGULAR_ERROR = np.deg2rad(45)
 
@@ -51,6 +53,7 @@ class LocalPlanner:
         self._global_path = GlobalPath()
         self._local_path = LocalPath(self._global_path)
         self._local_path_idx, self._distance_to_local_path = -1, 0
+        self._from_section, self._to_section = -1, -1
 
         self._traffic_light_color = TrafficLightState.GREEN  # or None # todo fill this
         # self._following_distance = -1  # todo set following distance
@@ -97,9 +100,10 @@ class LocalPlanner:
             self._global_path = GlobalPath(msg=msg)
             self._local_path = LocalPath(self._global_path, self.rules_enabled)
             self._set_local_path_idx()
-            self._create_paf_local_path_msg(
-                self._local_path.calculate_new_local_path(self._current_pose.position, self._current_speed)
+            path, self._from_section, self._to_section = self._local_path.calculate_new_local_path(
+                self._current_pose.position, self._current_speed
             )
+            self._create_paf_local_path_msg(path)
 
     # def _get_track_angle(self, s_index, l_index):
     #     if len(self._global_path.sections) == 0:
@@ -147,11 +151,14 @@ class LocalPlanner:
     def _create_paf_local_path_msg(self, path_msg=None, send_empty=False):
         if path_msg is None:
             path_msg = self._local_path.message
+        else:
+            self._local_path.message = path_msg
 
         if send_empty:
             path_msg.header.stamp = rospy.Time.now()
             path_msg.target_speed = []
             path_msg.points = []
+
         self._local_plan_publisher.publish(path_msg)
 
     def _loop_handler(self):
@@ -172,22 +179,42 @@ class LocalPlanner:
         elif not self._on_local_path():
             rospy.loginfo_throttle(5, "[local planner] not on local path, replanning")
             self._replan_local_path()
+        elif self._is_stopped() and len(self._local_path) > 10:
+            rospy.loginfo_throttle(5, "[local planner] car is waiting for event (red light / stop / yield)")
+            self._handle_clear_event()  # todo change this handler
         elif self._planner_at_end_of_local_path():
             rospy.loginfo_throttle(5, "[local planner] local planner is replanning (end of local path)")
             self._replan_local_path()
         else:
             rospy.loginfo_throttle(5, "[local planner] local planner on route, no need to replan")
 
-    def _replan_local_path(self):
-        msg = self._local_path.calculate_new_local_path(self._current_pose.position, self._current_speed)
-        self._create_paf_local_path_msg(msg)
+    def _is_waiting_for_clear_event(self):
+        pass  # todo implement this
 
-    def _handle_stop_event(self):
-        if self._allowed_from_stop():
-            rospy.sleep(3)  # todo remove
-            i1, i2 = self._local_path_idx, self._local_path_idx + 100
-            self._local_path.message.target_speed[i1:i2] = 50 / 3.6
-            self._create_paf_local_path_msg(self._local_path.message)
+    def _handle_clear_event(self):
+        rospy.sleep(3)  # todo remove this
+        self._local_path.message.target_speed = SpeedCalculator.remove_stop_event(
+            self._local_path.message.target_speed, max([self._local_path_idx - 5, 0])
+        )
+        to_section, _ = self._global_path.get_section_and_lane_indices(
+            self._current_pose.position, not_found_threshold_meters=50
+        )
+        if to_section < 0:
+            rospy.logerr("[local planner] cannot continue from here, current section unknown")
+            return
+        section: PafRouteSection
+        from_section = self._from_section
+        to_section += 5
+        for section in self._global_path.route.sections[from_section:to_section]:
+            section.signals = []
+
+        self._create_paf_local_path_msg()
+
+    def _replan_local_path(self):
+        msg, self._from_section, self._to_section = self._local_path.calculate_new_local_path(
+            self._current_pose.position, self._current_speed
+        )
+        self._create_paf_local_path_msg(msg)
 
     # def _get_current_trajectory(self):
     #     m_idx, l_idx, p_idx = self._set_current_indices()
@@ -283,7 +310,11 @@ class LocalPlanner:
     def _planner_at_end_of_local_path(self):
         if len(self._local_path) == 0:
             return True
-        return len(self._local_path) - self._local_path_idx < 300
+        return (
+            len(self._local_path) - self._local_path_idx < 500
+            and not dist_pts(self._local_path.message.points[-1], self._global_path.route.sections[-1].points[0])
+            < self.END_OF_ROUTE_REACHED_DIST
+        )
 
     def _planner_at_end_of_global_path(self):
         if len(self._global_path) == 0:
@@ -362,7 +393,6 @@ class LocalPlanner:
     def _end_of_route_handling(self):
         self._global_path = GlobalPath()
         rospy.Publisher("/paf/paf_validation/score/stop", Empty, queue_size=1).publish(Empty())
-        rospy.sleep(5)
         if self._current_speed < 1:
             # self._send_random_global_path_request()  # todo remove in production
             self._send_standard_loop_request()  # todo remove in production

paf_ros/paf_validation/src/top_down_view.py
@@ -104,7 +104,7 @@ class TopDownRosNode(object):
             self.producer.info_text[0] = int(self.velocity())
             self.producer.info_text[3] = self.location()
             if delta > 0:
-                rospy.logwarn_throttle(self.LOG_FPS_SECS, f"[top_down_view] fps={1 / delta}")
+                rospy.loginfo_throttle(self.LOG_FPS_SECS, f"[top_down_view] fps={np.round(1 / delta, 2)}")
             self._move_spectator()
             rate.sleep()
 